ed7503c5711cec019a2a898f22b166779ed88cfa
Your Name <vipa4ka@gmail.com>
2022-11-10 13:42:03 +0200

    feat: change color in body

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import './css/index.scss';
 
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+
+const btnStart = document.querySelector('[data-action="start"]');
+const btnStop = document.querySelector('[data-action="stop"]');
+const colorBody = document.querySelector('body');
+
+
+btnStart.addEventListener('click', onclickStart);
+btnStop.addEventListener('click', onclickStop);
+
+
+
+// генерация случайного числа
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
+function onclickStart(e) {
+    let randomNumber = randomIntegerFromInterval(0, 5);
+    colorBody.style.background=`${colors[randomNumber]}`
+  
+}
+
+function onclickStop() {
+    
+}
+
+
+
 
